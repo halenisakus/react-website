f7bb8302ae61ddcef095e863a14496b3fc2344d4
halenisakus <halenisakus@gmail.com>
2020-09-19 21:51:21 +0300

    edit Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-          TRVL <i className="fab fa-typo3"> </i>{' '}
+          HNK <i className="fab fa-typo3"> </i>{' '}
         </Link>{' '}
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />{' '}
@@ -35,7 +35,7 @@ function Navbar() {
         <ul className={click ? 'nav-menu active ' : 'nav-menu'}>
           <li className="nav-item">
             <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-              Menu{' '}
+              Home{' '}
             </Link>{' '}
           </li>{' '}
           <li className="nav-item">
